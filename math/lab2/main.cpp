4712c227063995a2b9772fa6a91b89399c6591cf
KaluginaMarina <marin.kalugin@gmail.com>
2019-09-24 12:32:44 +0300

    [math lab2] код Хаффмана

--- math/lab2/main.cpp
+++ math/lab2/main.cpp
@@ -59,75 +59,103 @@ struct node {                   // узел дерева
     }
 };
 
+std::map<char, symbol> symbols;                       // char -- символ, symbol -- структура с информацией о символе
+
+void make_codes_huffman(std::shared_ptr<node> node, char code) {
+    for (auto &s : node->symbs) {
+        auto iter = symbols.find(s.symb);
+        iter->second.code_huff += code;
+    }
+    if (!node->child1) {
+        return;
+    }
+    make_codes_huffman(node->child1, '0');
+    if (!node->child2) {
+        return;
+    }
+    make_codes_huffman(node->child2, '1');
+}
+
+
 void huffman(std::map<char, symbol> &symbs) {        // Функция, высчитывающаяя код Хаффмана
     std::vector<std::shared_ptr<node>> nodes;
     for (auto &symb : symbs) {
         std::vector<symbol> s;
         s.push_back(symb.second);
-        nodes.insert(nodes.end(), std::make_shared<node>(s, symb.second.p, std::shared_ptr<node>(), std::shared_ptr<node>()));
+        nodes.insert(nodes.end(),
+                     std::make_shared<node>(s, symb.second.p, std::shared_ptr<node>(), std::shared_ptr<node>()));
     }
+    // создание дерева
     for (int i = 0; i < symbs.size() - 2; ++i) {
         // найдем 1 минимальную, сохраним:
         double min = 2;
         std::shared_ptr<node> min_node;
-        for (auto & node : nodes){
+        for (auto &node : nodes) {
             if (node->p <= min) {
                 min = node->p;
                 min_node = node;
             }
         }
         // удалим из вектора:
-       nodes.erase( std::remove_if(nodes.begin(), nodes.end(), [&](std::shared_ptr<node> const & n) {
-                         return n == min_node;
-                     }),
-                     nodes.end());
+        nodes.erase(std::remove_if(nodes.begin(), nodes.end(), [&](std::shared_ptr<node> const &n) {
+                        return n == min_node;
+                    }),
+                    nodes.end());
         // найдем 2 минимальную, сохраним:
         min = 2;
         std::shared_ptr<node> min2_node;
-        for (auto & node : nodes){
+        for (auto &node : nodes) {
             if (node->p <= min) {
                 min = node->p;
                 min2_node = node;
             }
         }
         // удалим из вектора:
-        nodes.erase( std::remove_if(nodes.begin(), nodes.end(), [&](std::shared_ptr<node> const & n) {
-                         return n == min2_node;
-                     }),
-                     nodes.end());
+        nodes.erase(std::remove_if(nodes.begin(), nodes.end(), [&](std::shared_ptr<node> const &n) {
+                        return n == min2_node;
+                    }),
+                    nodes.end());
         // создадим узел:
         std::vector<symbol> concat(min2_node->symbs.begin(), min2_node->symbs.end());
         concat.insert(concat.end(), min_node->symbs.begin(), min_node->symbs.end());
-        nodes.insert(nodes.end(), std::make_shared<node>(std::move(concat), min2_node->p + min_node->p, min_node, min2_node));
-
+        nodes.insert(nodes.end(),
+                     std::make_shared<node>(std::move(concat), min2_node->p + min_node->p, min_node, min2_node));
+    }
+    char code = '0';
+    for (auto &node : nodes) {
+        make_codes_huffman(node, code);
+        code = (code == '0') ? '1' : '0';
     }
 }
 
 
 int main() {
-    std::map<char, symbol> symbs;                       // char -- символ, symbol -- структура с информацией о символе
     std::ifstream in("../input/1984_500.txt");
     if (in.is_open()) {                                  // чтение посимвольно с файла и сохранение количества каждого типа символов в map
         int sum = 0;                                     // кол-во символов
         while (!in.eof()) {
             char c = in.get();
-            auto iter = symbs.find(c);
-            if (iter != symbs.end()) {                   // если символ уже встречался -- увеличим счетчик
+            auto iter = symbols.find(c);
+            if (iter != symbols.end()) {                   // если символ уже встречался -- увеличим счетчик
                 iter->second.n++;
             } else {
                 symbol s = symbol(c);                    // если символ еще не встречался, создадим элемент в мапе
-                symbs.insert({c, s});
+                symbols.insert({c, s});
             }
             sum++;
         }
-        for (auto iter = symbs.begin(); iter != symbs.end(); iter++) {   // расчет вероятностей
-            iter->second.p = (double)iter->second.n / (double)sum;
+        for (auto iter = symbols.begin(); iter != symbols.end(); iter++) {   // расчет вероятностей
+            iter->second.p = (double) iter->second.n / (double) sum;
         }
     } else {
         std::cout << "Файл не найден";
     }
     in.close();
 
-    huffman(symbs);
+    huffman(symbols);
+
+    for (auto &s : symbols) {
+        std::cout << s.first << " " << s.second.code_huff << "\n";
+    }
     return 0;
 }
